dfd7a75c4c6d6a040bdf8ad5788f3a1346b4d620
Morten Scheel <ms@findforsikring.dk>
2017-09-08 13:58:09 +0200

    Implemented commands in Symfony

--- app.php
+++ app.php
@@ -2,11 +2,17 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+use Kuli\FileCheckCommand;
+use Kuli\GithubCloneCommand;
+use Kuli\LaravelInstallCommand;
 use Symfony\Component\Console\Application;
 
-$application = new Application('echo', '1.0.0');
-$command = new \Kuli\FileCheckCommand();
-
-$application->add($command);
-
+define('KULI_VERSION', '0.1.2');
+$application = new Application('Kuli', KULI_VERSION);
+$commands = [
+    new FileCheckCommand(),
+    new GithubCloneCommand(),
+    new LaravelInstallCommand()
+];
+$application->addCommands($commands);
 $application->run();
